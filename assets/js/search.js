c325fa9c87a02be588ccac8c826b3047bc7ebca2
victor-marchesini <victor-marchesini@users.noreply.github.com>
2021-04-14 02:21:31 +0000

    deploy: 8421aefd344a6a41b580fbea2171e6be775741ba

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/myfastpage/assets/js/search-data.json', true);
+    request.open('GET', '/usp_eah5002/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
